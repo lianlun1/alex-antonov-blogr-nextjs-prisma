93ef5b2aa8515c0c7541630896d91a9fbc60d05c
lianlun1 <93830765+lianlun1@users.noreply.github.com>
2022-06-11 14:07:12 +0400

    First commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,36 @@
 import React from "react"
-import { GetServerSideProps } from "next"
+import { GetServerSideProps, GetStaticProps } from "next"
 import Layout from "../components/Layout"
 import Post, { PostProps } from "../components/Post"
+import prisma from "../lib/prisma";
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const feed = [
-    {
-      id: "1",
-      title: "Prisma is the perfect ORM for Next.js",
-      content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
-      published: false,
+export const getStaticProps: GetStaticProps = async () => {
+  const feed = await prisma.post.findMany({
+    where: { published: true },
+    include: {
       author: {
-        name: "Nikolas Burk",
-        email: "burk@prisma.io",
+        select: { name: true },
       },
     },
-  ]
-  return { props: { feed } }
+  });
+  return { props: { feed } };
 }
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const feed = [
+//     {
+//       id: "1",
+//       title: "Prisma is the perfect ORM for Next.js",
+//       content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
+//       published: false,
+//       author: {
+//         name: "Nikolas Burk",
+//         email: "burk@prisma.io",
+//       },
+//     },
+//   ]
+//   return { props: { feed } }
+// }
 
 type Props = {
   feed: PostProps[]
